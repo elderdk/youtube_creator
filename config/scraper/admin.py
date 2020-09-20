1f29f602e12d97d427f9cbb4bf170ed6b89cc17c
DanielK <elder.dk@gmail.com>
2020-09-20 12:06:06 +0900

    register comment

--- config/scraper/admin.py
+++ config/scraper/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Submission
+from .models import Submission, Comment
 
 # Register your models here.
 @admin.register(Submission)
 class SubmissionAdmin(admin.ModelAdmin):
     passdate_hierarchy = 'scraped_time'
     list_display = ('sub_id', 'scraped_time', 'title')
+
+admin.site.register(Comment)
