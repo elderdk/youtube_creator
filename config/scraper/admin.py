6dcc3a35445ca0b1a9a9c6dcf8bac6b642058614
DanielK <elder.dk@gmail.com>
2020-09-21 22:30:17 +0900

    fix issues with txt files remaining after download

--- config/scraper/admin.py
+++ config/scraper/admin.py
@@ -1,11 +1,13 @@
 import re
 from datetime import datetime
 from pathlib import Path
+import shutil
 
 from django.contrib import admin
 from django.utils.text import slugify
 from django.contrib import messages
 from django.utils.translation import ngettext
+from django.http import FileResponse
 
 from .models import Comment, Submission
 
@@ -14,7 +16,7 @@ from .models import Comment, Submission
 @admin.register(Submission)
 class SubmissionAdmin(admin.ModelAdmin):
     passdate_hierarchy = 'scraped_time'
-    list_display = ('sub_id', 'scraped_time', 'title')
+    list_display = ('sub_id', 'subreddit', 'score', 'title', 'scraped_time')
 
     actions = ['download']
 
@@ -24,12 +26,11 @@ class SubmissionAdmin(admin.ModelAdmin):
             sentences = re.findall('\s+[^.!?]*[.!?]', msg)
             return '\n'.join([s.strip() for s in sentences])
 
-        file_list = list()
-
         for sub in submissions:
             slug_title = slugify(sub.title, allow_unicode=True)
             dt = datetime.strftime(sub.created_time, '%Y%m%d')
-            fname = Path("./scraper/tmp").joinpath(f"{dt}_{sub.subreddit}_{sub.sub_id}_{slug_title[:20]}.txt")
+            tmp_folder = Path("./scraper/tmp")
+            fname = tmp_folder.joinpath(f"{dt}_{sub.subreddit}_{sub.sub_id}_{slug_title[:20]}.txt")
             with fname.open(mode='w') as f:
                 text = """
                 {title}
@@ -47,16 +48,29 @@ class SubmissionAdmin(admin.ModelAdmin):
                     text_body = _divide_per_line(sub.selftext)
                 )
 
-                f.write(text)                
+                f.write(text)     
 
-                self.message_user(request, ngettext(
-                    '%d file downloaded',
-                    '%d files downloaded',
-                    submissions,
-                    ) % len(submissions), messages.SUCCESS)
-                    # updated,
-                    # ) % updated, messages.SUCCESS)
+        self.message_user(request, ngettext(
+            '%d file downloaded',
+            '%d files downloaded',
+            submissions,
+            ) % len(submissions), messages.SUCCESS)
 
+        # Below is for creating a zip file and auto-initiating download.
+        # Below should be moved to another file later for aesthetics.
+        
+        # Currently the zip file is saved in the root folder (i.e. /config)
+        # This should be fixed so that download.zip is saved and cleaned up after downloading.
+        zf_file = shutil.make_archive(tmp_folder.joinpath('download').name, 'zip', tmp_folder)
+        response = FileResponse(open(zf_file, 'rb'), as_attachment=True)
+        response['Content-Disposition'] = f"attachment; filename={zf_file.split('/')[-1]}"
+
+        # Clean the tmp_folder
+        for f in tmp_folder.glob('*.txt'):
+            f.unlink()
+
+        return response
+        
     download.short_description = "Download the selected files"
 
 admin.site.register(Comment)
